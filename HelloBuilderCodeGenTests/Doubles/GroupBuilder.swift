f1f2b0824db3e37042a77541e8cbf1f4c932829b
acicartagena <acicartagena@gmail.com>
2021-01-07 18:41:30 +1100

    Run sourcery then update code to use builders

--- HelloBuilderCodeGenTests/Doubles/GroupBuilder.swift
+++ HelloBuilderCodeGenTests/Doubles/GroupBuilder.swift
@@ -6,9 +6,35 @@ import Foundation
 @testable import HelloBuilderCodeGen
 
 extension Group: AutoTestBuilder {
-    static private let membersTestDefault: [User] = []
+    static private let membersTestDefault: [User] = {
+        guard let user = try? UserBuilder().with(email: "me@email.com").build() else { return [] }
+        return [user]
+    }()
     static private let nameTestDefault = "Group"
 }
 
 // sourcery:inline:Group.AutoTestBuilder
+final class GroupBuilder {
+	private var members: [User]? = []
+	private var name: String? = "Group"
+
+	func with(members: [User]) -> GroupBuilder {
+		self.members = members
+		return self
+	}
+	func with(name: String) -> GroupBuilder {
+		self.name = name
+		return self
+	}
+
+	func build() throws -> Group {
+		guard let members = self.members else { throw AutoTestBuilderError.missingValue }
+		guard let name = self.name else { throw AutoTestBuilderError.missingValue }
+		return Group(
+			members:members,
+			name:name
+		)
+	}
+
+}
 // sourcery:end
