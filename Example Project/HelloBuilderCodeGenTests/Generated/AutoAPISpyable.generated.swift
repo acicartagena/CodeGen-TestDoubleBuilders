bb92b1b6db3d7a61ad750eee9df66cab4601ca7b
acicartagena <acicartagena@gmail.com>
2021-01-08 22:37:01 +1100

    fix compiler issues

--- Example Project/HelloBuilderCodeGenTests/Generated/AutoAPISpyable.generated.swift
+++ Example Project/HelloBuilderCodeGenTests/Generated/AutoAPISpyable.generated.swift
@@ -1,5 +1,6 @@
 // Generated using Sourcery 1.0.2 — https://github.com/krzysztofzablocki/Sourcery
 // DO NOT EDIT
+@testable import HelloBuilderCodeGen
 
 final class MessagesAPISpy: MessagesAPI {
     var fetchAllResult: Result<[Message], APIError>?
